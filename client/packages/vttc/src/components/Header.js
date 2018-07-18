a576aeb0981a636e4aa5ce28535ddab86a975172
nli2 <a-nli2@expedia.com>
2018-07-18 14:10:51 -0700

    header size

--- client/packages/vttc/src/components/Header.js
+++ client/packages/vttc/src/components/Header.js
@@ -7,7 +7,7 @@ import actions from 'utils/actions';
 import { lookupSelector } from 'utils/selectors';
 import { range } from 'ramda';
 
-const s = m => ({ position: 'absolute', height: m ? '100px' : '150px', width: '10%' });
+const s = m => ({ position: 'absolute', height: m ? '75px' : '150px', width: '10%' });
 const s1 = m => ({ ...s(m), left: 0, background: 'linear-gradient(to left, rgba(42, 27, 112,0) 20%, rgba(42, 27, 112,1) 100%)' });
 const s2 = m => ({ ...s(m), right: 0, background: 'linear-gradient(to right, rgba(42, 27, 112, 0) 20%, rgba(42, 27, 112, 1) 100%)' });
 const n = 11;
@@ -19,7 +19,7 @@ const Mq = ({ lookup, isMobile }) =>
     <div class="f aic m1">
       {r2.map(x =>
         <Link to="/">
-          <img width={isMobile ? '85' : '130'} height={isMobile ? '85' : '130'} src={cdurl(lookup, 'header', x)} alt="" />  
+          <img width={isMobile ? '65' : '130'} height={isMobile ? '85' : '130'} src={cdurl(lookup, 'header', x)} alt="" />  
         </Link>
       )}
     </div>
