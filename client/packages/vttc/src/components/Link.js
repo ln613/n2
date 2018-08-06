4c88ab2da82d749ec6b62d65c6b2a8022cc0e70f
ln613 <ln613@hotmail.com>
2018-08-06 05:54:25 -0700

    link

--- client/packages/vttc/src/components/Link.js
+++ client/packages/vttc/src/components/Link.js
@@ -10,5 +10,9 @@ export default p =>
     <li><a href="https://www.ittf.com/rankings/" target="_new">ITTF World Ranking</a></li><br/>
 
     <li><a href="http://cttsl.sports.cn/" target="_new">Offical Website for China Table Tennis Super League</a></li>
-    
+
+    <li><a href="https://butterflyonline.com/" target="_new">Butterfly</a></li><br/>
+
+    <li><a href="http://www.joolausa.com/" target="_new">Joola</a></li><br/>
+
   </div>  
