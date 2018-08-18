8703c2ce569066397225e616d9f135b136cd5ff5
ln613 <ln613@hotmail.com>
2018-08-18 10:44:12 -0700

    div d

--- client/packages/vttc/src/components/Shop.js
+++ client/packages/vttc/src/components/Shop.js
@@ -2,22 +2,32 @@ import React from 'react';
 import { connect } from 'no-redux';
 import { compose } from 'recompose';
 import actions from 'utils/actions';
-import { productsSelector } from 'utils/selectors';
+import { lookupSelector } from 'utils/selectors';
 import { cdurl, withLoad, findById } from 'utils';
 import { withMobile } from 'utils/comps';
 import CatMenu from './CatMenu';
 
 const brands = ['729', 'addidas', 'andro', 'asics', 'avalox', 'butterfly', 'cornilleau', 'darker', 'dawei', 'dhs', 'donic', 'Dr.Neubauer', 'gewo', 'haifu', 'joola', 'kokutaku', 'ktl', 'li-ning', 'Mizuno', 'nittaku', 'palio', 'stiga', 'sword', 'tibhar', 'tsp', 'victas', 'xiom', 'yasaka', 'yinhe'];
+const imgs = [2,3,4];
 
-const Shop = ({ isMobile }) =>
+const Shop = ({ isMobile, lookup }) =>
   <div class="p16 fv">
-    <h1>Shop - Coming Soon</h1>
+    <h1>Shop</h1>
     <hr />
     <h2>We are the official dealer of Butterfly and Joola, and we carry the following brands.</h2>
     <h2>我们是 Butterfly 和 Joola 加拿大总代理。我们也代理以下品牌.</h2>
     {isMobile ? <div class="tac pv8">{brands.map(c1)}</div> : <div class={`fw pb16 ${isMobile ? '' : 'ph8'}`}>{brands.map(b => <div class="f w20 p8">{c1(b)}</div>)}</div>}
+    <hr/>
+    <h2>本月特价 On Sale</h2>
+    {isMobile ? <div class="tac pv8">{imgs.map(n => c2(lookup, n))}</div> : <div class={`fw pb16 ${isMobile ? '' : 'ph8'}`}>{imgs.map(n => <div class="f w20 p8">{c2(lookup, n)}</div>)}</div>}
+    <hr/>
+    <h2>Online shopping coming soon</h2>
   </div>
 
-export default withMobile(Shop);
+export default compose(
+  connect(lookupSelector),
+  withMobile
+)(Shop);
 
 const c1 = b => <div><img src={'/images/brands/' + b + '.jpg'} /></div>;
+const c2 = (l, n) => <div><img class="w100" src={cdurl(l, 'slider-5', n)} /></div>;
