3e4c5a42f679c4270c0a6733478b07768ffa4be0
Nan Li <Nan.Li@bcaa.com>
2019-01-25 13:46:43 -0800

    group header

--- client/packages/vttc/src/components/TMenu.js
+++ client/packages/vttc/src/components/TMenu.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const TMenu = ({ id, isSingle, isMobile, page }) =>
+const TMenu = ({ id, isSingle, isGroup, isMobile, page }) =>
   <div class={`ui ${isMobile ? 'four item' : 'vertical'} menu`}>
-    <Link class={`item ${page === 'tournament' ? 'active' : ''}`} to={'/tournaments/' + id}>{isSingle ? 'Players' : 'Teams'}</Link>
+    <Link class={`item ${page === 'tournament' ? 'active' : ''}`} to={'/tournaments/' + id}>{isSingle ? 'Players' : (isGroup ? 'Groups' : 'Teams')}</Link>
     <Link class={`item ${page === 'schedule' ? 'active' : ''}`} to={'/schedule/' + id}>Schedule</Link>
     <Link class={`item ${page === 'standing' ? 'active' : ''}`} to={'/standing/' + id}>Standing</Link>
     <Link class={`item ${page === 'stats' ? 'active' : ''}`} to={'/stats/' + id}>Stats</Link>
--- client/packages/vttc/src/components/Tournament.js
+++ client/packages/vttc/src/components/Tournament.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { connect } from '@ln613/state';
 import { compose } from 'recompose';
+import { isNil } from 'ramda';
 import actions from 'utils/actions';
 import { tournamentSelector } from 'utils/selectors';
 import { withLoad, withLoadForce, withParams } from '@ln613/compose';
 import { Table, withMobile } from '@ln613/ui/semantic';
+import { get } from '@ln613/util';
 import TMenu from './TMenu';
 
 const Tournament = ({ lookup, tournament, id, isMobile }) =>
   <div class={`p16 ${isMobile ? 'fv' : 'f'}`}>
-    <TMenu id={id} isSingle={tournament.isSingle} isMobile={isMobile} page="tournament" />
+    <TMenu id={id} isSingle={tournament.isSingle} isGroup={!isNil(get('teams[0].group')(tournament))} isMobile={isMobile} page="tournament" />
     <div class={`${isMobile ? '' : 'ph32'} fv`}>
       <h1>{tournament.name}</h1>
       <hr/>
