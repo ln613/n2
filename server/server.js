9eec79ec5d03aea05a3661ebdae1396cd090aff4
Nan Li <Nan.Li@bcaa.com>
2019-02-14 13:06:05 -0800

    build path

--- server/server.js
+++ server/server.js
@@ -16,8 +16,7 @@ if (!isProd)
 
 api.initdb(mongoURL);
 
-app.use(express.static('client/packages/vttc/build'));
-app.use(express.static('client/packages/admin/build'));
+app.use(express.static('client/build'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
