7bdbc0d6173b2bf878b3c2714825ed9732f23b8a
ln613 <ln613@hotmail.com>
2019-05-17 01:04:02 -0700

    cd

--- src/admin/Convert.js
+++ src/admin/Convert.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import { isDev } from 'util';
+import React, { Fragment } from 'react';
+import { withMount, withState } from '@ln613/compose';
+import { tap } from '@ln613/util';
+import { compose, withHandlers } from 'recompose';
+import { Label, Button, Icon } from 'semantic-ui-react';
 
-export default () =>
-  <form action={isDev ? 'http://localhost:3000' : 'https://4mlzcqfe60.execute-api.us-west-2.amazonaws.com/dev/'} method="post" enctype="multipart/form-data">
-    <input type="file" name="f1" />
-    <button type="submit">submit</button>
-  </form>
+const Convert = ({ file, selectFile, convert }) =>
+  <div>
+    <div>
+      <Label width="4" as="label" htmlFor="file" color="orange" size="big" className="cp">
+        <Icon name="file" />
+        Select a file...
+      </Label>
+      <div>{(file || {}).name}</div>
+      <input id="file" hidden type="file" onChange={selectFile} />
+    </div>
+    <div class="mt8">
+      <Button primary onClick={() => convert(file)}>Convert &amp; Upload</Button>
+    </div>
+  </div>
+
+export default compose(
+  withState('file'),
+  withHandlers(({
+    selectFile: p => () => p.setFile(((document.getElementById('file') || {}).files || [])[0]),
+    convert: () => async file => {
+      const fd = new FormData();
+      fd.append('upload_preset', 'baicr6sd');
+      fd.append('file', file);
+      const c = await post('https://api.cloudinary.com/v1_1/vttc/raw/upload', fd, true);
+      const p = await post('https://api.cloudconvert.com/v1/process', { inputformat: 'docx', outputformat: 'png' }, false, true);
+      const s = await post(p.url, { outputformat: 'png', input: 'download', file: c.url, wait: true });
+      tap(s)
+    }
+  })),
+)(Convert);
+
+const post = (url, params, isUpload, withKey) => fetch(url, {
+  method: 'post',
+  headers: {
+    ...(withKey ? { Authorization: 'Bearer eOgfamMWkKHQSkHwerVGWc6jvrWUrCC2SOh7J3oA8IrsYVkNuJoNyJI10fNbaBfZ' } : {}),
+    ...(isUpload ? {} : { 'Content-Type': 'application/json' }),
+  },
+  body: isUpload ? params : JSON.stringify(params)
+}).then(r => r.json());
