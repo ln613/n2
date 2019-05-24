5d6f0ef183179ddd10060f8612461c9e8dd10c1c
Nan Li <Nan.Li@bcaa.com>
2019-05-24 12:40:15 -0700

    canDisplaySchedule

--- src/admin/Tournament.js
+++ src/admin/Tournament.js
@@ -12,13 +12,15 @@ import { tap } from '@ln613/util';
 import { withRouter, Link } from "react-router-dom";
 import { withSuccess } from 'utils/ui';
 
+const NUMBERS = ['', 'one', 'two', 'three', 'four', 'five', 'six'];
+
 const b1 = p =>
   <Fragment>
     {p.t.isSingle
       ? <Link class="item" to={`/admin/singleplayers/${p.t.id}`}>Players</Link>
       : <Link class="item" to={`/admin/teams/${p.t.id}`}>Teams</Link>
     }
-    {!isEmpty(p.t.schedules) ? <Link class="item" to={`/admin/schedules/${p.t.id}`}>Schedules</Link> : null}
+    {p.canDisplaySchedule ? <Link class="item" to={`/admin/schedules/${p.t.id}`}>Schedules</Link> : null}
     {/* <Link class="item" to={`/admin/games/${tournament.id}`}>Games</Link> */}
     {p.canGenerateGroup ? <Link class="item" onClick={() => !p.isLoading && p.postGengroup({ id: p.id })} to="#">Generate Groups</Link> : null}
     {p.canGenerateSchedule ? <Link class="item" onClick={() => !p.isLoading && p.postGenrr({ id: p.id })} to="#">Generate Schedule</Link> : null}
@@ -28,9 +30,9 @@ const b1 = p =>
 
 const Tournament = p =>
   <div>
-    <h1>Tournament - {+tap(p).t.id ? p.t.name : 'Add New'}</h1>
+    <h1>Tournament - {+p.t.id ? p.t.name : 'Add New'}</h1>
     <hr />
-    {+p.t.id ? <div class={`ui three item menu`}>{b1(p)}</div> : null}
+    {+p.t.id ? <div class={`ui ${NUMBERS[p.numOfButtons]} item menu`}>{b1(p)}</div> : null}
     <TextBox name="tournament.id" disabled />
     <TextBox name="tournament.name" fluid />
     <CheckBox name="tournament.isSingle" label="Is Single?" />
@@ -49,10 +51,14 @@ export default compose(
   withLoadForce('tournament'),
   withMount(p => p.setForm(find(x => x.id == +p.id, (p.tournaments || [])) || { id: +p.id, name: '',  startDate: '', startDate2: '', isSingle: false, ratingDate: '' }, { path: 'tournament' })),
   withProps(({ standing, t }) => ({
+    canDisplaySchedule: !isEmpty(t.schedules),
+    canGenerateSchedule: !isEmpty(t.isSingle ? t.players : t.teams) && isEmpty(t.schedules),
+    canGenerateSchedule2: !t.has2half && !isEmpty(t.teams) && isNil(t.teams[0].group) && !isEmpty(t.schedules) && !isEmpty(standing) && standing.length > 2,
     canGenerateGroup: !t.isSingle && !isEmpty(t.teams) && isNil(t.teams[0].group) && isEmpty(t.games) && isEmpty(t.schedules),
-    canGenerateSchedule: !isEmpty(t.teams) && isEmpty(t.schedules),
-    canGenerateSchedule2: !t.has2half && !isEmpty(t.teams) && !isEmpty(t.schedules) && !isEmpty(standing) && standing.length > 2,
     canGenerateKO: !isEmpty(t.teams) && !isNil(t.teams[0].group) && !isEmpty(t.schedules),
+  })),
+  withProps(p => ({
+    numOfButtons: [p.canDisplaySchedule, p.canGenerateSchedule, p.canGenerateSchedule2, p.canGenerateGroup, p.canGenerateKO].filter(x => x === true).length + 1
   })),
   withSuccess(['tour', 'genrr', 'gengroup'], () => alert('Saved'), () => alert('Error happened!')),
   withRouter,
