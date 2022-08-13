f7bedffa32a19ba6aadf8a2a153eb12f7210b649
ln613 <ln613@hotmail.com>
2022-08-13 12:54:27 -0700

    bold news

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,7 +28,10 @@ const Home = ({ isMobile }) =>
   <div>
     <div class="p16">
       <h2>VTTC Monthly Tournaments</h2>
-      <div style={{paddingRight: '16px'}}>
+      <div class="news">
+        All new monthly tournaments series! Starting from Sep., every month at VTTC there will be a tournament of different type to meet the need of different players of all levels. In Sep. there will be a team event, Oct. will be singles and Nov. is round robin. Registration is now open. Please refer to the registration form for more detials.
+      </div>
+      <div class="news" style={{paddingRight: '16px'}}>
         全新理念, 全新设计. 为满足所有乒乓球迷浓厚的热情, 本中心推出一系列的比赛, 每月举办一场不同类型的比赛来满足不同球友的需求, 使得我们的比赛更加丰富多彩. 这里是9月和10月比赛的信息和报名表.
         <br/><a href="/docs/2022 VTTC Monthly Teams Sep.pdf">9月团体赛报名表 (Sep. Teams Challenge Registration Form)</a>
         <br/><a href="/docs/2022 VTTC Monthly Singles Oct.pdf">10月单打报名表 (Oct. Singles Registration Form)</a>
