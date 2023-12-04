a8556f65894b387dc9098f6a467af1c8af64e728
ln613 <ln613@hotmail.com>
2023-12-03 18:15:14 -0800

    hidden

--- src/components/Tournaments.js
+++ src/components/Tournaments.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { connect } from '@ln613/state';
-import { compose } from 'recompose';
-import { pick } from 'ramda';
-import actions from 'utils/actions';
-import { tournamentsSelector } from 'utils/selectors';
-import { withLoad } from '@ln613/compose';
-import { Table } from '@ln613/ui/semantic';
+import React from 'react'
+import { connect } from '@ln613/state'
+import { compose, withProps } from 'recompose'
+import { pick } from 'ramda'
+import actions from 'utils/actions'
+import { tournamentsSelector } from 'utils/selectors'
+import { withLoad } from '@ln613/compose'
+import { Table } from '@ln613/ui/semantic'
 
-const Tournaments = ({ lookup, tournaments }) =>
+const Tournaments = ({ lookup, tournaments }) => (
   <div class="p16 fv">
     <h1>Tournaments</h1>
-    <hr/>
-    <Table name="tournaments" data={tournaments.map(pick(['id', 'year', 'name']))} link>
-      <td key="id" hidden/>   
-      <td key="year" title="Y / M" class="wsn"/>   
+    <hr />
+    <Table
+      name="tournaments"
+      data={tournaments.map(pick(['id', 'year', 'name']))}
+      link
+    >
+      <td key="id" hidden />
+      <td key="year" title="Y / M" class="wsn" />
     </Table>
   </div>
+)
 
 export default compose(
   connect(tournamentsSelector, actions),
-  withLoad('tournaments')
-)(Tournaments);
+  withLoad('tournaments'),
+  withProps(p => ({
+    tournaments: (p.tournaments || []).filter(x => !x.isHidden),
+  }))
+)(Tournaments)
