4e43e7fba32910529111ca5e7d0e48f9b05a0af7
ln613 <ln613@hotmail.com>
2024-02-21 15:56:25 -0800

    pusher

--- functions/api.js
+++ functions/api.js
@@ -1,42 +1,75 @@
-const fs = require('fs');
-const { connectDB, get, getIdName, getById, search, getPlayerRating, getPlayerGames, cdVersion, getNewGameId, getDetail, update } = require('./utils/db');
-const { res } = require('./utils');
+const fs = require('fs')
+const {
+  connectDB,
+  get,
+  getIdName,
+  getById,
+  search,
+  getPlayerRating,
+  getPlayerGames,
+  cdVersion,
+  getNewGameId,
+  getDetail,
+  update,
+} = require('./utils/db')
+const { res } = require('./utils')
 const { updatePlayerSex } = require('./bl/player')
+const pusher = require('./utils/pusher')
 
 module.exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const { test, doc, id, fields, prop, val, idname, player_rating, rating_date, player_games, lookup, newgameid, folder, detail } = event.queryStringParameters;
-  await connectDB();
-  let r = {};
+  context.callbackWaitsForEmptyEventLoop = false
+  const {
+    test,
+    doc,
+    id,
+    fields,
+    prop,
+    val,
+    idname,
+    player_rating,
+    rating_date,
+    player_games,
+    lookup,
+    newgameid,
+    folder,
+    detail,
+    pusherChannel,
+    pusherEvent,
+    pusherData,
+  } = event.queryStringParameters
+  await connectDB()
+  let r = {}
 
   if (lookup) {
-    const version = await cdVersion();
-    const cats = await get('cats');
-    r = { cats, cdVersion: version };
+    const version = await cdVersion()
+    const cats = await get('cats')
+    r = { cats, cdVersion: version }
   } else if (newgameid) {
-    r = await getNewGameId();
+    r = await getNewGameId()
   } else if (idname) {
-    r = await getIdName(doc);
+    r = await getIdName(doc)
   } else if (id && doc) {
-    r = await getById(doc, id);
+    r = await getById(doc, id)
   } else if (fields || prop) {
-    r = await search(doc, prop, val, fields);
+    r = await search(doc, prop, val, fields)
   } else if (player_rating) {
-    r = await getPlayerRating(id, rating_date);
+    r = await getPlayerRating(id, rating_date)
   } else if (player_games) {
-    r = await getPlayerGames(id);
+    r = await getPlayerGames(id)
   } else if (detail) {
-    r = await getDetail(id);
+    r = await getDetail(id)
   } else if (doc) {
-    r = await get(doc);
+    r = await get(doc)
   } else if (folder) {
     //r = fs.readdirSync(process.cwd());
     r = fs.writeFileSync(process.cwd() + '/tmp/1.txt', 'abc')
+  } else if (push) {
+    await pusher.trigger(pusherChannel, pusherEvent, JSON.parse(pusherData))
   } else if (test) {
     const p1 = await getById('players', 642)
-    updatePlayerSex(p1);
+    updatePlayerSex(p1)
     update('players', p1)
   }
 
-  return res(r);
-};
+  return res(r)
+}
