4630fb8f15758bb031e6a4e2879470ce09ae55ba
ln613 <ln613@hotmail.com>
2025-01-09 00:55:13 -0800

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,7 +68,7 @@ const yt = (s, m) => (
 const Home = ({ isMobile }) => (
   <div>
     <div className="p16">
-      <h2 style={{ color: 'red' }}>
+      {/* <h2 style={{ color: 'red' }}>
         2025 New Year Cup
       </h2>
       <div>Dec. 28, 2024</div>
@@ -76,7 +76,7 @@ const Home = ({ isMobile }) => (
         <a href="/docs/2025 New Years Cup.pdf">
           Registration Form
         </a>
-      </div>
+      </div> */}
       {/* <h2 style={{ color: 'gold' }}>VTTC Monday Mini Tourney Series</h2>
       <div>
         Every 2 weeks starting from Feb 26th. Fund raising for future junior
